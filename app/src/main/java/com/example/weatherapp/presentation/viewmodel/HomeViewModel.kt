9f951704c9b70475fe67bc2da66564ec1acec67e
bayazidsustami <bayazidsustamy@gmail.com>
2024-02-09 00:35:27 +0800

    feat : cleanup code

--- app/src/main/java/com/example/weatherapp/presentation/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/weatherapp/presentation/viewmodel/HomeViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.weatherapp.presentation.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.weatherapp.common.UIState
